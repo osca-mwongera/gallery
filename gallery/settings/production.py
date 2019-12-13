7b2ce848bad573312c9d1f435edd50b3880f3ad6
Osca Mwongera <oscamwongera@gmail.com>
2019-12-13 14:48:29 +0300

    set debug to True

--- gallery/settings/production.py
+++ gallery/settings/production.py
@@ -5,7 +5,7 @@ import dj_database_url
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['galleriaapp.herokuapp.com']
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
